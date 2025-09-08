d77244e3850418bfb69b02ea98163a2b0cab83ff
Owen Sterling <j.owen.sterling@outlook.com>
2025-09-08 14:25:26 -0700

    Revert state update

--- src/lib/globals.svelte.ts
+++ src/lib/globals.svelte.ts
@@ -7,7 +7,6 @@ export function startCountdown(timer: Timer) {
 		} else {
 			clearInterval(timer.intervalId);
 			timer.timeLeft = 0;
-			timer.intervalId = undefined
 			if (
 				Notification.permission === 'granted' &&
 				Object.keys(all_timers).length > 0
