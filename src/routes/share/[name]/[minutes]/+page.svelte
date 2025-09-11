419cc27a23e0d91d23cba8a48094a15c7f71be1d
JOwen-ster <jowen.sterling@csu.fullerton.edu>
2025-09-10 18:05:45 -0700

    Update

--- src/routes/share/[name]/[minutes]/+page.svelte
+++ src/routes/share/[name]/[minutes]/+page.svelte
@@ -10,13 +10,14 @@
 
 		display_notification(`Set ${data.name} for ${data.minutes} minutes.`)
 
-		setTimeout(() => {
-			goto("/");
-		}, 1000);
+		// setTimeout(() => {
+		// 	goto("/");
+		// }, 1000);
 	});
 </script>
 
 <h1>Creating timer "{data.name}" for {data.minutes} minutes...</h1>
+<a href="/">test</a>
 
 <style>
     h1 {
